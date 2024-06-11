5f5dd153905bd25c397c97d5889ccf58cf43f362
rom05 <rbertrand.ecoex.cnrs@gmail.com>
2024-06-11 13:09:35 +0200

    10000 bootstrapped models & significance od dependent effect

--- R/1_full_model/0_ModelFit.R
+++ R/1_full_model/0_ModelFit.R
@@ -3,7 +3,7 @@
 list.of.packages <- c(
     "doParallel", "parallel","foreach","pdftools","plotly",
     "pbapply","dplyr", "tidyr", "parallel",
-    "scales","effects","psych", "glmmTMB", "lme4", "lmerTest","here","rlist","ggtext","gridExtra","grid","lattice","viridis") 
+    "scales","effects","psych", "glmmTMB", "lme4", "lmerTest","here","rlist","ggtext","gridExtra","grid","lattice","viridis","performance") 
 
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -13,11 +13,11 @@ if(length(new.packages)) install.packages(new.packages)
 sapply(list.of.packages, require, character.only = TRUE)
 ################################################################################
 #define the data repository
-dir_in="/home/rbertrand/W/Bioshift/GD_study/data_Brunno_v02022024/" #to change accordingly to the location of the data
-dir_out="/home/rbertrand/W/Bioshift/GD_study/boot_analysis/REMLF2" #to change. It's the repository where the results are saved
+dir.in="/home/rbertrand/W/Bioshift/GD_study/data_Brunno_v02022024/" #to change accordingly to the location of the data
+dir.out="/home/rbertrand/W/Bioshift/GD_study/boot_analysis/REMLF2" #to change. It's the repository where the results are saved
 
 # Load data
-setwd(dir_in)
+setwd(dir.in)
 mydataset <- read.csv2("gen_data_final_fonseca.csv",sep=",",dec=".",h=T) #file path in GitHub: /adaptive-potential/Data
 
 #Data selection
@@ -137,13 +137,14 @@ gam_velxGDxedge1 <- as.formula(
     (1 | Class)")
 s1=69 #the initial random seed
 nB=12000 #the number of bootstraps
+nCPU=15
 
 #setting a local cluster
 my.cluster2 <- parallel::makeCluster(
     nCPU, 
     type = "PSOCK"
 )
-clusterExport(cl = my.cluster2, varlist = c("s1","nB","glmmTMB","mydatatogo","gam_velxGDxedge1","ranef"))
+clusterExport(cl = my.cluster2, varlist = c("s1","nB","glmmTMB","mydatatogo","gam_velxGDxedge1","ranef","check_singularity"))
 
 #register cluster
 doParallel::registerDoParallel(cl = my.cluster2)
@@ -184,7 +185,7 @@ ex=pblapply(1:nB, function(i) {
                      weights = weight_obs_spp,
                      REML=F,
                      data = mydatatogo2)
-    
+
     r2=round(MuMIn::r.squaredGLMM(gamX1),2)
     r2=data.frame(r2m=r2[1,1],
                   r2c=r2[1,2],
@@ -192,6 +193,7 @@ ex=pblapply(1:nB, function(i) {
                   sd_GD=sd(mydatatogo2$GD),
                   moy_VC=mean(mydatatogo2$vel_abs), 
                   sd_VC=sd(mydatatogo2$vel_abs),
+                  singu=check_singularity(gamX1)[[1]],
                   nB=i)
     
     coeff=data.frame(summary(gamX1)$coeff$cond,
@@ -207,7 +209,7 @@ ex=pblapply(1:nB, function(i) {
 
 parallel::stopCluster(cl = my.cluster2)
 
-nom="allW2"
+nom="allW"
 randEff=rlist::list.rbind(lapply(ex,"[[",3))
 coeff=rlist::list.rbind(lapply(ex,"[[",2))
 r2=rlist::list.rbind(lapply(ex,"[[",1))
@@ -223,113 +225,287 @@ write.table(randEff,paste0("randEff_",nom,".csv"),
             sep=";",dec=".",row=F)
 
 #analysis of raw bootstrap output
-setwd(dir.out)
-resR2_ok_file <- read.csv2("summary_R2.csv",sep=";",dec=".",h=T)
-res_ok_file <- read.csv2("summary_coeff.csv",sep=";",dec=".",h=T)
-
-if(all(file.exists(resR2_ok_file,res_ok_file))){
-    
-    resR2_ok_file <- read.table(resR2_ok_file, sep=";", dec=".")
-    res_ok_file <- read.table(res_ok_file, sep=";", dec=".")
-    
-} else {
-    
-    #mod=c("allW","CEW","TEW","LEW","allW2")
-    mod='allW2'
-    
-    a=1
-    s1=10 #the seed to make reproducible random staff
-    nB=10000
-    
-    
-    for(i in 1:length(mod)){
-        print(a)
-        r2=read.csv2(paste0("R2_",mod[i],".csv"),sep=";",dec=".")
-        #r2$nB=1:nrow(r2)
-        r2a=subset(r2,is.na(r2m)==F)
-        print(paste(mod[i],': ',nrow(r2a),sep=""))
-        if(nrow(r2a)>=nB){ #random selection of nB bootstrap model (criteria: model convergence)
-            set.seed(s1)
-            r2a=r2a[sample(1:nrow(r2a),size=nB,replace=F),]
-        }
-        if(nrow(r2a)<nB){
-            print(paste0("sampling size is less than ",nB," bootstraps"))
-        }else{
-            resR2=data.frame(type="r2m",moy=mean(r2a$r2m),sd=sd(r2a$r2m),median=median(r2a$r2m),q025=quantile(r2a$r2m,probs=0.025),q975=quantile(r2a$r2m,probs=0.975))
-            resR2=rbind(resR2,data.frame(type="r2c",moy=mean(r2a$r2c),sd=sd(r2a$r2c),median=median(r2a$r2c),q025=quantile(r2a$r2c,probs=0.025),q975=quantile(r2a$r2c,probs=0.975)))
-            #resR2$n_sing=nrow(subset(r2a,sing==T))
-            resR2$model=mod[i]
-            
-            c1=read.csv2(paste("coeff_",mod[i],".csv",sep=""),sep=";",dec=".",h=T)
-            #c1$nB=rep(1:nrow(r2),each=nrow(c1)/6000)
-            c1=merge(c1,data.frame(nB=r2a$nB),by.x="nB",by.y="nB")
-            rr3=c1
-            
-            res=data.frame(var=names(tapply(rr3$Estimate,rr3$var,mean)),moy=tapply(rr3$Estimate,rr3$var,mean),sd=tapply(rr3$Estimate,rr3$var,sd),median=tapply(rr3$Estimate,rr3$var,median),q025=tapply(rr3$Estimate,rr3$var,quantile,probs=0.025),
-                           p975=tapply(rr3$Estimate,rr3$var,quantile,probs=0.975), pv.inf0=tapply(rr3$Estimate,rr3$var,test2,mu=0),pv.sup0=tapply(rr3$Estimate,rr3$var,test1,mu=0))
-            res$model=mod[i]
-            if(a==1){
-                resR2_ok=resR2
-                res_ok=res
-            }
-            else{
-                resR2_ok=rbind(resR2_ok,resR2)
-                res_ok=rbind(res_ok,res)
-            }
-            
-            a=a+1
-        }
-    }
-    
-    write.table(resR2_ok,
-                "summary_R2.csv",
-                sep=";",dec=".",row=F) 
-    
-    #R2 statistic computed from 5000 boostrap model:
-    #type= R2 type (r2m= marginal R2; r2c= conditional R2)
-    #moy= mean R2 value
-    #sd= R2 standard deviation value
-    #median= median R2 value
-    #q025= 2.5% quantile of the bootstrap distribution
-    #q975= 97.5% quantile of the bootstrap distribution
-    #n_sing= number of singular model
-    #model= model name abbreviation
-    
-    write.table(res_ok,
-                "summary_coeff.csv",
-                sep=";",dec=".",row=F) 
-    #coefficient statistics
-    #Var =  Term of the model
-    #moy = mean effect computed from the 5000 bootstrap model
-    #sd = standard deviation computed from the 5000 bootstrap model
-    #med = median effect computed from the 5000 bootstrap model
-    #q025 = 2.5% quantile of the bootstrap coefficient distribution
-    #q975 = 97.5% quantile of the bootstrap coefficient distribution
-    #pv.sup0 = p-value of the bootstrap test testing the significance of the effect (H0= coefficient equal to 0; H1= coefficient greater than 0)
-    #pv.inf0 = p-value of the bootstrap test testing the significance of the effect (H0= coefficient equal to 0; H1= coefficient lower than 0)
-    #model= model name abbreviation
-    
-    # #significant positive effect
-    # subset(res_ok,model=="allW" & pv.sup0<0.05)
-    # subset(res_ok,model=="CEW" & pv.sup0<0.05)
-    # subset(res_ok,model=="LEW" & pv.sup0<0.05)
-    # subset(res_ok,model=="TEW" & pv.sup0<0.05)
-    # subset(res_ok,model=="allW2" & pv.sup0<0.05)
-    # 
-    # #significant negative effect
-    # subset(res_ok,model=="allW" & pv.inf0<0.05)
-    # subset(res_ok,model=="CEW" & pv.inf0<0.05)
-    # subset(res_ok,model=="LEW" & pv.inf0<0.05)
-    # subset(res_ok,model=="TEW" & pv.inf0<0.05)
-    # subset(res_ok,model=="allW2" & pv.inf0<0.05)
-    # 
-    # #non-significant negative effect
-    # subset(res_ok,model=="allW" & pv.inf0>=0.05 & pv.sup0>=0.05)
-    # subset(res_ok,model=="CEW" & pv.inf0>=0.05 & pv.sup0>=0.05)
-    # subset(res_ok,model=="LEW" & pv.inf0>=0.05 & pv.sup0>=0.05)
-    # subset(res_ok,model=="TEW" & pv.inf0>=0.05 & pv.sup0>=0.05)
-    # subset(res_ok,model=="allW2" & pv.inf0>=0.05 & pv.sup0>=0.05)
+test1=function(x,mu=0){
+    return(1-(length(x[x>mu])/length(x)))
 }
+
+#bootstrap test: H0= coefficient equal to 0; H1= coefficient lower than 0
+test2=function(x,mu=0){
+    return(1-(length(x[x<mu])/length(x)))
+}
+
+
+mod='allW'
+a=1
+s1=10 #the seed to make reproducible random staff
+nB=10000
+setwd(dir.out)
+
+for(i in 1:length(mod)){
+    print(a)
+    r2=read.csv2(paste0("R2_",mod[i],".csv"),sep=";",dec=".")
+    #r2$nB=1:nrow(r2)
+    r2a=subset(r2,is.na(r2m)==F)
+    print(paste(mod[i],': ',nrow(r2a),sep=""))
+    if(nrow(r2a)>=nB){ #random selection of nB bootstrap model (criteria: model convergence)
+        set.seed(s1)
+        r2a=r2a[sample(1:nrow(r2a),size=nB,replace=F),]
+    }
+    if(nrow(r2a)<nB){
+        print(paste0("sampling size is less than ",nB," bootstraps"))
+    }else{
+        resR2=data.frame(type="r2m",moy=mean(r2a$r2m),sd=sd(r2a$r2m),median=median(r2a$r2m),q025=quantile(r2a$r2m,probs=0.025),q975=quantile(r2a$r2m,probs=0.975))
+        resR2=rbind(resR2,data.frame(type="r2c",moy=mean(r2a$r2c),sd=sd(r2a$r2c),median=median(r2a$r2c),q025=quantile(r2a$r2c,probs=0.025),q975=quantile(r2a$r2c,probs=0.975)))
+        #resR2$n_sing=nrow(subset(r2a,sing==T))
+        resR2$model=mod[i]
+        
+        c1=read.csv2(paste("coeff_",mod[i],".csv",sep=""),sep=";",dec=".",h=T)
+        #c1$nB=rep(1:nrow(r2),each=nrow(c1)/6000)
+        c1=merge(c1,data.frame(nB=r2a$nB),by.x="nB",by.y="nB")
+        rr3=c1
+        
+        res=data.frame(var=names(tapply(rr3$Estimate,rr3$var,mean)),moy=tapply(rr3$Estimate,rr3$var,mean),sd=tapply(rr3$Estimate,rr3$var,sd),median=tapply(rr3$Estimate,rr3$var,median),q025=tapply(rr3$Estimate,rr3$var,quantile,probs=0.025),
+                       p975=tapply(rr3$Estimate,rr3$var,quantile,probs=0.975), pv.inf0=tapply(rr3$Estimate,rr3$var,test2,mu=0),pv.sup0=tapply(rr3$Estimate,rr3$var,test1,mu=0))
+        res$model=mod[i]
+        if(a==1){
+            resR2_ok=resR2
+            res_ok=res
+        }
+        else{
+            resR2_ok=rbind(resR2_ok,resR2)
+            res_ok=rbind(res_ok,res)
+        }
+        
+        a=a+1
+    }
+}
+
+write.table(resR2_ok,
+            "summary_R2.csv",
+            sep=";",dec=".",row=F) 
+
+#R2 statistic computed from 5000 boostrap model:
+#type= R2 type (r2m= marginal R2; r2c= conditional R2)
+#moy= mean R2 value
+#sd= R2 standard deviation value
+#median= median R2 value
+#q025= 2.5% quantile of the bootstrap distribution
+#q975= 97.5% quantile of the bootstrap distribution
+#n_sing= number of singular model
+#model= model name abbreviation
+
+write.table(res_ok,
+            "summary_coeff.csv",
+            sep=";",dec=".",row=F) 
+#coefficient statistics
+#Var =  Term of the model
+#moy = mean effect computed from the 5000 bootstrap model
+#sd = standard deviation computed from the 5000 bootstrap model
+#med = median effect computed from the 5000 bootstrap model
+#q025 = 2.5% quantile of the bootstrap coefficient distribution
+#q975 = 97.5% quantile of the bootstrap coefficient distribution
+#pv.sup0 = p-value of the bootstrap test testing the significance of the effect (H0= coefficient equal to 0; H1= coefficient greater than 0)
+#pv.inf0 = p-value of the bootstrap test testing the significance of the effect (H0= coefficient equal to 0; H1= coefficient lower than 0)
+#model= model name abbreviation
+
+
+# #significant positive effect
+subset(res_ok,model=="allW" & pv.sup0<0.05)
+
+# #significant negative effect
+subset(res_ok,model=="allW" & pv.inf0<0.05)
+
+# #non-significant negative effect
+subset(res_ok,model=="allW" & pv.inf0>=0.05 & pv.sup0>=0.05)
+
+### Test climate velocity value for which GD is significant
+#### allW2 model
+nB=r2a$nB
+c1=read.csv2("coeff_allW.csv",
+             sep=";",dec=".",h=T)
+dsel=mydatatogo
+v1=seq(round(min(dsel$vel_abs),1),round(max(dsel$vel_abs),1),by=0.1)
+
+my.cluster2 <- parallel::makeCluster(
+    nCPU, 
+    type = "PSOCK"
+)
+
+clusterExport(cl = my.cluster2, varlist = c("c1","dsel","v1","test1","test2"))
+
+#register cluster
+doParallel::registerDoParallel(cl = my.cluster2)
+
+ex=pblapply(nB,function(i){
+    gc(reset=T)
+    print(i)
+    c1a=subset(c1,nB==i)
+    eff=c1a$Estimate[c1a$var=="scale(GD)"]+(c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD)"]*((v1-mean(dsel$vel_abs))/sd(dsel$vel_abs)))
+    tmp=data.frame(vel_abs=v1,GDeff_CE=eff)
+    eff=c1a$Estimate[c1a$var=="scale(GD)"]+c1a$Estimate[c1a$var=="scale(GD):ParamLE"]+((c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD)"]+c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD):ParamLE"])*((v1-mean(dsel$vel_abs))/sd(dsel$vel_abs)))
+    tmp$GDeff_LE=eff
+    eff=c1a$Estimate[c1a$var=="scale(GD)"]+c1a$Estimate[c1a$var=="scale(GD):ParamTE"]+((c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD)"]+c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD):ParamTE"])*((v1-mean(dsel$vel_abs))/sd(dsel$vel_abs)))
+    tmp$GDeff_TE=eff
+    tmp$nB=i
+    return(tmp)
+}, cl = my.cluster2)
+
+parallel::stopCluster(cl = my.cluster2)
+
+ex <- do.call("rbind",ex)
+
+#summary statistics
+resCE=data.frame(vel_abs=names(tapply(ex$GDeff_CE,ex$vel_abs,mean)),
+                 moy=tapply(ex$GDeff_CE,ex$vel_abs,mean),
+                 sd=tapply(ex$GDeff_CE,ex$vel_abs,sd),
+                 median=tapply(ex$GDeff_CE,ex$vel_abs,median),
+                 q025=tapply(ex$GDeff_CE,ex$vel_abs,quantile,probs=0.025),
+                 p975=tapply(ex$GDeff_CE,ex$vel_abs,quantile,probs=0.975), 
+                 pv.inf0=tapply(ex$GDeff_CE,ex$vel_abs,test2,mu=0),
+                 pv.sup0=tapply(ex$GDeff_CE,ex$vel_abs,test1,mu=0))
+
+resLE=data.frame(vel_abs=names(tapply(ex$GDeff_LE,ex$vel_abs,mean)),
+                 moy=tapply(ex$GDeff_LE,ex$vel_abs,mean),
+                 sd=tapply(ex$GDeff_LE,ex$vel_abs,sd),
+                 median=tapply(ex$GDeff_LE,ex$vel_abs,median),
+                 q025=tapply(ex$GDeff_LE,ex$vel_abs,quantile,probs=0.025),
+                 p975=tapply(ex$GDeff_LE,ex$vel_abs,quantile,probs=0.975),
+                 pv.inf0=tapply(ex$GDeff_LE,ex$vel_abs,test2,mu=0),
+                 pv.sup0=tapply(ex$GDeff_LE,ex$vel_abs,test1,mu=0))
+
+resTE=data.frame(vel_abs=names(tapply(ex$GDeff_TE,ex$vel_abs,mean)),
+                 moy=tapply(ex$GDeff_TE,ex$vel_abs,mean),
+                 sd=tapply(ex$GDeff_TE,ex$vel_abs,sd),
+                 median=tapply(ex$GDeff_TE,ex$vel_abs,median),
+                 q025=tapply(ex$GDeff_TE,ex$vel_abs,quantile,probs=0.025),
+                 p975=tapply(ex$GDeff_TE,ex$vel_abs,quantile,probs=0.975),
+                 pv.inf0=tapply(ex$GDeff_TE,ex$vel_abs,test2,mu=0),
+                 pv.sup0=tapply(ex$GDeff_TE,ex$vel_abs,test1,mu=0))
+
+
+write.table(resTE,
+            "summary_GDeff_allW_TE.csv",
+            sep=";",dec=".",row=F) 
+write.table(resCE,
+            "summary_GDeff_allW_CE.csv",
+            sep=";",dec=".",row=F) 
+write.table(resLE,
+            "summary_GDeff_allW_LE.csv",
+            sep=";",dec=".",row=F) 
+
+# #significant positive effect
+subset(resCE,pv.sup0<0.05)
+# #significant negative effect
+subset(resCE,pv.inf0<0.05)
+# #non-significant negative effect
+subset(resCE,pv.inf0>=0.05 & pv.sup0>=0.05)
+
+
+# #significant positive effect
+subset(resLE,pv.sup0<0.05)
+# #significant negative effect
+subset(resLE,pv.inf0<0.05)
+# #non-significant negative effect
+subset(resLE,pv.inf0>=0.05 & pv.sup0>=0.05)
+
+
+# #significant positive effect
+subset(resTE,pv.sup0<0.05)
+# #significant negative effect
+subset(resTE,pv.inf0<0.05)
+# #non-significant negative effect
+subset(resTE,pv.inf0>=0.05 & pv.sup0>=0.05)
+
+### Test GD value for which the climate velocity effect is significant
+c1=read.csv2("coeff_allW.csv",sep=";",dec=".",h=T)
+dsel=mydatatogo
+v1=seq(round(min(dsel$GD),3),round(max(dsel$GD),3),by=0.001)
+
+my.cluster2 <- parallel::makeCluster(
+    nCPU, 
+    type = "PSOCK"
+)
+
+clusterExport(cl = my.cluster2, varlist = c("c1","dsel","v1","test1","test2"))
+
+ex=pblapply(nB,function(i){
+    gc(reset=T)
+    print(i)
+    c1a=subset(c1,nB==i)
+    eff=c1a$Estimate[c1a$var=="scale(vel_abs)"]+(c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD)"]*((v1-mean(dsel$GD))/sd(dsel$GD)))
+    tmp=data.frame(GD=v1,VAeff_CE=eff)
+    eff=c1a$Estimate[c1a$var=="scale(vel_abs)"]+c1a$Estimate[c1a$var=="scale(vel_abs):ParamLE"]+((c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD)"]+c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD):ParamLE"])*((v1-mean(dsel$GD))/sd(dsel$GD)))
+    tmp$VAeff_LE=eff
+    eff=c1a$Estimate[c1a$var=="scale(vel_abs)"]+c1a$Estimate[c1a$var=="scale(vel_abs):ParamTE"]+((c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD)"]+c1a$Estimate[c1a$var=="scale(vel_abs):scale(GD):ParamTE"])*((v1-mean(dsel$GD))/sd(dsel$GD)))
+    tmp$VAeff_TE=eff
+    tmp$nB=i
+    return(tmp)
+}, cl = my.cluster2)
+
+parallel::stopCluster(cl = my.cluster2)
+
+ex <- do.call("rbind",ex)
+
+#summary statistictics
+resCE=data.frame(GD=names(tapply(ex$VAeff_CE,ex$GD,mean)),
+                 moy=tapply(ex$VAeff_CE,ex$GD,mean),
+                 sd=tapply(ex$VAeff_CE,ex$GD,sd),
+                 median=tapply(ex$VAeff_CE,ex$GD,median),
+                 q025=tapply(ex$VAeff_CE,ex$GD,quantile,probs=0.025),
+                 p975=tapply(ex$VAeff_CE,ex$GD,quantile,probs=0.975),
+                 pv.inf0=tapply(ex$VAeff_CE,ex$GD,test2,mu=0),
+                 pv.sup0=tapply(ex$VAeff_CE,ex$GD,test1,mu=0))
+
+resLE=data.frame(GD=names(tapply(ex$VAeff_LE,ex$GD,mean)),
+                 moy=tapply(ex$VAeff_LE,ex$GD,mean),
+                 sd=tapply(ex$VAeff_LE,ex$GD,sd),
+                 median=tapply(ex$VAeff_LE,ex$GD,median),
+                 q025=tapply(ex$VAeff_LE,ex$GD,quantile,probs=0.025),
+                 p975=tapply(ex$VAeff_LE,ex$GD,quantile,probs=0.975),
+                 pv.inf0=tapply(ex$VAeff_LE,ex$GD,test2,mu=0),
+                 pv.sup0=tapply(ex$VAeff_LE,ex$GD,test1,mu=0))
+
+resTE=data.frame(GD=names(tapply(ex$VAeff_TE,ex$GD,mean)),
+                 moy=tapply(ex$VAeff_TE,ex$GD,mean),
+                 sd=tapply(ex$VAeff_TE,ex$GD,sd),
+                 median=tapply(ex$VAeff_TE,ex$GD,median),
+                 q025=tapply(ex$VAeff_TE,ex$GD,quantile,probs=0.025),
+                 p975=tapply(ex$VAeff_TE,ex$GD,quantile,probs=0.975),
+                 pv.inf0=tapply(ex$VAeff_TE,ex$GD,test2,mu=0),
+                 pv.sup0=tapply(ex$VAeff_TE,ex$GD,test1,mu=0))
+
+write.table(resTE,
+            "summary_VAeff_allW_TE.csv",
+            sep=";",dec=".",row=F) 
+write.table(resLE,
+            "summary_VAeff_allW_LE.csv",
+            sep=";",dec=".",row=F) 
+write.table(resCE,
+            "summary_VAeff_allW_CE.csv",
+            sep=";",dec=".",row=F) 
+
+# #significant positive effect
+subset(resCE,pv.sup0<0.05)
+# #significant negative effect
+subset(resCE,pv.inf0<0.05)
+# #non-significant negative effect
+subset(resCE,pv.inf0>=0.05 & pv.sup0>=0.05)
+
+
+# #significant positive effect
+subset(resLE,pv.sup0<0.05)
+# #significant negative effect
+subset(resLE,pv.inf0<0.05)
+# #non-significant negative effect
+subset(resLE,pv.inf0>=0.05 & pv.sup0>=0.05)
+
+
+# #significant positive effect
+subset(resTE,pv.sup0<0.05)
+# #significant negative effect
+subset(resTE,pv.inf0<0.05)
+# #non-significant negative effect
+subset(resTE,pv.inf0>=0.05 & pv.sup0>=0.05)
 
 
 
