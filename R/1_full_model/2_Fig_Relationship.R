458990c56221267bfde26d766a2cce2b7c8e32e2
rom05 <rbertrand.ecoex.cnrs@gmail.com>
2024-06-12 17:11:33 +0200

    small cor

--- R/1_full_model/2_Fig_Relationship.R
+++ R/1_full_model/2_Fig_Relationship.R
@@ -1269,6 +1269,5 @@ png(paste0(dir.out,"/fig3_GDeffectOnVIC_v2.png"),unit="cm",width=27,height=11,re
 #plot_annotation(tag_levels = list(c('(a)','(b)','(c)','(d)'))) # figure tags
 dev.off()
 
-####Important: the final fig3_v1 has been formatted on ppt
 
 
